1f148a9dc8041fd995f824a360657149eaa19b82
Sin0001 <bboyliljie@gmail.com>
2021-11-01 17:04:01 +0800

    feat: add update record function

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
+//瀏覽首頁
 app.get('/', (req, res) => {
   Record.find()
     .lean()
@@ -37,14 +38,41 @@ app.get('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
+//新增record頁面
 app.get('/records/new', (req, res) => {
   return res.render('new')
 })
 
+//送出新增record
 app.post('/records', (req, res) => {
   const { name, date, amount, category} = req.body
   return Record.create({ name, date, amount, category }) //存入資料庫
     .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
+
+//修改record頁面
+app.get('/records/:id/edit', (req, res) => {
+  const id = req.params.id
+  Record.findById(id)
+    .lean()
+    .then((record) => res.render('edit', {record}))
+    .catch(error => console.log(error))
+})
+
+//送出修改record
+app.post('/records/:id/edit', (req, res) => {
+  const id = req.params.id
+  const { name, date, amount, category } = req.body
+  return Record.findById(id)
+    .then(record => {
+      record.name = name
+      record.date = date
+      record.amount = amount
+      record.categoty = category
+      return record.save()
+    })
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
